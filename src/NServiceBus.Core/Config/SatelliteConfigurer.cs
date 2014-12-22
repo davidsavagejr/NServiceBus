fe110c56721a071c53490c3d37bf94e47691dee9
Andreas Öhlund <andreas.ohlund@particular.net>
2014-12-22 10:01:21 +0100

    Register the ExecuteSatelliteHandlerBehavior in the container

--- src/NServiceBus.Core/Config/SatelliteConfigurer.cs
+++ src/NServiceBus.Core/Config/SatelliteConfigurer.cs
@@ -1,5 +1,6 @@
 namespace NServiceBus.Config
 {
+    using NServiceBus.Unicast.Transport;
     using Satellites;
 
     class SatelliteConfigurer : INeedInitialization
@@ -7,6 +8,9 @@ namespace NServiceBus.Config
         public void Customize(BusConfiguration configuration)
         {
             Configure.ForAllTypes<ISatellite>(configuration.Settings.GetAvailableTypes(), t => configuration.RegisterComponents(c => c.ConfigureComponent(t, DependencyLifecycle.SingleInstance)));
+
+            //need to register it here since we're hacking the main pipeline
+            configuration.RegisterComponents(c => c.ConfigureComponent<ExecuteSatelliteHandlerBehavior>(DependencyLifecycle.InstancePerCall));
         }
     }
 }
