ac5a6dcf913eeb5c7b8033e50dc3b1b91396f12c
Andreas Öhlund <andreas.ohlund@particular.net>
2014-12-22 09:52:06 +0100

    Cloning the pipeline for satellites to avoid collection modified ex

---
+++ src/NServiceBus.Core/Licensing/NotifyOnInvalidLicenseBehavior.cs
@@ -0,0 +1,34 @@
+﻿namespace NServiceBus
+{
+    using System;
+    using System.Diagnostics;
+    using Logging;
+    using Pipeline;
+    using Pipeline.Contexts;
+
+    class NotifyOnInvalidLicenseBehavior : IBehavior<IncomingContext>
+    {
+        public void Invoke(IncomingContext context, Action next)
+        {
+            context.PhysicalMessage.Headers[Headers.HasLicenseExpired] = true.ToString().ToLower();
+
+            next();
+
+            if (Debugger.IsAttached)
+            {
+                Log.Error("Your license has expired");
+            }
+        }
+
+        static ILog Log = LogManager.GetLogger<NotifyOnInvalidLicenseBehavior>();
+
+        public class Registration : RegisterStep
+        {
+            public Registration()
+                : base("LicenseReminder", typeof(NotifyOnInvalidLicenseBehavior), "Enforces the licensing policy")
+            {
+                InsertBefore(WellKnownStep.AuditProcessedMessage);
+            }
+        }
+    }
+}
