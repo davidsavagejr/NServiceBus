451f0b114dba9611ebf4854eded319c09b1e152b
Andreas Öhlund <andreas.ohlund@particular.net>
2015-01-17 11:38:36 +0200

    Making serialization integration tests a unit test

---
+++ src/NServiceBus.Core.Tests/Serializers/SerializeMessagesBehaviorTests.cs
@@ -0,0 +1,50 @@
+﻿namespace NServiceBus.Core.Tests.Serializers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using NServiceBus.Pipeline.Contexts;
+    using NServiceBus.Serialization;
+    using NServiceBus.Unicast;
+    using NServiceBus.Unicast.Messages;
+    using NUnit.Framework;
+
+    public class SerializeMessagesBehaviorTests
+    {
+        [Test]
+        public void Should_set_content_type_header()
+        {
+            var behavior = new SerializeMessagesBehavior
+            {
+                MessageSerializer = new FakeSerializer("myContentType")
+            };
+            var transportMessage = new TransportMessage("xyz", new Dictionary<string, string>());
+            var context = new RootContext(null);
+
+            context.Set(transportMessage);
+            behavior.Invoke(new OutgoingContext(context, new SendOptions("test"), new LogicalMessage(new Dictionary<string, string>(), null)), () => { });
+
+            Assert.AreEqual("myContentType", transportMessage.Headers[Headers.ContentType]);
+        }
+
+        public class FakeSerializer : IMessageSerializer
+        {
+            public FakeSerializer(string contentType)
+            {
+                ContentType = contentType;
+            }
+
+            public void Serialize(object message, Stream stream)
+            {
+                
+            }
+
+            public object[] Deserialize(Stream stream, IList<Type> messageTypes = null)
+            {
+                throw new NotImplementedException();
+            }
+
+            public string ContentType { get; private set; }
+        }
+    }
+}
--- src/NServiceBus.Core.Tests/Unicast/Sending.cs
+++ src/NServiceBus.Core.Tests/Unicast/Sending.cs
@@ -6,62 +6,8 @@
     using Rhino.Mocks;
 
     [TestFixture]
-    class When_sending_a_message_with_databusProperty : using_the_unicastBus
-    {
-        [Test]
-        public void Should_sent_if_only_one_message_is_in_the_same_send()
-        {
-            RegisterMessageType<CommandWithDataBusPropertyMessage>();
-
-            bus.Send(new CommandWithDataBusPropertyMessage());
-        }
-    }
-
-    [TestFixture]
-    class When_sending_a_event_message : using_the_unicastBus
-    {
-        [Test]
-        public void Should_get_an_error_messages()
-        {
-            RegisterMessageType<EventMessage>();
-            Assert.Throws<InvalidOperationException>(() => bus.Send(new EventMessage()));
-        }
-    }
-
-    [TestFixture]
     class When_sending_any_message : using_the_unicastBus
     {
-        [Test]
-        public void The_content_type_should_be_set()
-        {
-            bus.OutgoingHeaders["MyStaticHeader"] = "StaticHeaderValue";
-            RegisterMessageType<TestMessage>();
-            bus.Send(new TestMessage());
-
-
-            messageSender.AssertWasCalled(x => x.Send(Arg<TransportMessage>.Matches(m =>
-                m.Headers[Headers.ContentType] == "text/xml" &&
-                m.Headers["MyStaticHeader"] == "StaticHeaderValue"), Arg<SendOptions>.Is.Anything));
-        }
-
-        [Test]
-        public void It_should_be_persistent_by_default()
-        {
-            RegisterMessageType<TestMessage>();
-            bus.Send(new TestMessage());
-
-            messageSender.AssertWasCalled(x => x.Send(Arg<TransportMessage>.Matches(m => m.Recoverable), Arg<SendOptions>.Is.Anything));
-        }
-
-        [Test]
-        public void Should_set_the_reply_to_address()
-        {
-            RegisterMessageType<TestMessage>();
-            bus.Send(new TestMessage());
-
-            messageSender.AssertWasCalled(x => x.Send(Arg<TransportMessage>.Is.Anything, Arg<SendOptions>.Matches(o => o.ReplyToAddress == configure.LocalAddress)));
-        }
-
         [Test]
         public void Should_generate_a_conversation_id()
         {
@@ -82,19 +28,7 @@
             messageSender.AssertWasCalled(x => x.Send(Arg<TransportMessage>.Matches(m => m.Headers[Headers.ConversationId] == "my order id"), Arg<SendOptions>.Is.Anything));
         }
 
-        [Test, Ignore("Needs refactoring to make testing possible")]
-        public void Should_propagate_the_incoming_replyTo_address_if_requested()
-        {
-            var addressOfIncomingMessage = Address.Parse("Incoming");
-
-            //todo - add a way to set the context from out tests
-
-            //bus.PropagateReturnAddressOnSend = true;
-            RegisterMessageType<TestMessage>();
-            bus.Send(new TestMessage());
-
-            messageSender.AssertWasCalled(x => x.Send(Arg<TransportMessage>.Matches(m => m.ReplyToAddress == addressOfIncomingMessage), Arg<SendOptions>.Is.Anything));
-        }
+       
     }
 
     [TestFixture]
